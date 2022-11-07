e50120473a3d11e2978b618493c6e35781fe714c
Seb <seebeesbt@gmail.com>
2022-11-07 23:21:49 +0000

    Auto-create table

--- database.js
+++ database.js
@@ -2,6 +2,9 @@ const config = require("./config.json");
 const db = require('better-sqlite3')(config.databaseName || "Claire.db");
 
 const GET_ALL_SENTENCES_CONTAINING_WORD = "SELECT * FROM MessagePairs WHERE LOWER(message) LIKE ?";
+const CREATE_TABLE = "CREATE TABLE IF NOT EXISTS MessagePairs (message TEXT, reply TEXT)"
+
+db.exec(CREATE_TABLE);
 
 function getAllRepliesForWord(word) {
     return db.prepare(GET_ALL_SENTENCES_CONTAINING_WORD).all("% " + word + " %");
